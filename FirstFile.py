9a679b4682faceb9ce75852337f3c858df856b0b
dpkprmr <38376323+dpkprmr@users.noreply.github.com>
2020-07-07 18:46:06 +0530

    updated the file with more expressions

--- FirstFile.py
+++ FirstFile.py
@@ -1,8 +1,12 @@
 
 a=10
 b=20
-
+ for Python Project
 print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
 print(b**a)
 
-print("This is a demo text for Jenkins and Git Integration")
+print("This is a demo text for Jenkins and Git Integration for Python Project")
